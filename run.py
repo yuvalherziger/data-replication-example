ee4027bff0f0a37d2cef5e365f6895b09192f38a
yuvalherziger <yuvalhrz@gmail.com>
2025-02-05 15:25:42 +0200

    Make stream resumable

--- run.py
+++ run.py
@@ -14,7 +14,10 @@ SOURCE_MONGODB_URI = os.getenv("SOURCE_MONGODB_URI")
 
 SOURCE_DB = os.getenv("SOURCE_DB", "globalData")
 TARGET_DB = os.getenv("TARGET_DB", "data")
-TARGET_CLUSTERS_URIS = [value for key, value in os.environ.items() if key.startswith("TARGET_MONGODB_URI_")]
+TARGET_CLUSTERS_URIS = [
+    value for key, value in os.environ.items() if key.startswith("TARGET_MONGODB_URI_")
+]
+
 
 class ChangeStreamHandler:
     def __init__(self, source_uri: str, target_uris: List[str]):
@@ -34,9 +37,8 @@ class ChangeStreamHandler:
             logger.exception(f"Failed to connect to MongoDB clusters")
             raise
 
-    async def write_to_target(self, change):
+    async def write_to_target(self, change, resume_token):
         try:
-            logger.info(change)
             coll_name = change.get("ns", {}).get("coll")
             operation_type = change.get("operationType")
             _id = change.get("documentKey", {}).get("_id")
@@ -45,7 +47,10 @@ class ChangeStreamHandler:
             if operation_type == "insert":
                 full_doc = change.get("fullDocument")
                 await asyncio.gather(
-                    *[c[TARGET_DB][coll_name].insert_one(full_doc) for c in self.target_clients]
+                    *[
+                        c[TARGET_DB][coll_name].insert_one(full_doc)
+                        for c in self.target_clients
+                    ]
                 )
 
             elif operation_type == "update":
@@ -57,16 +62,25 @@ class ChangeStreamHandler:
 
                 if update_desc.get("removedFields"):
                     update_doc["$unset"] = {
-                        field: ""
-                        for field in update_desc.get("removedFields")
+                        field: "" for field in update_desc.get("removedFields")
                     }
                 await asyncio.gather(
-                    *[c[TARGET_DB][coll_name].update_one({"_id": _id}, update_doc) for c in self.target_clients]
+                    *[
+                        c[TARGET_DB][coll_name].update_one({"_id": _id}, update_doc)
+                        for c in self.target_clients
+                    ]
                 )
             elif operation_type == "delete":
                 await asyncio.gather(
-                    *[c[TARGET_DB][coll_name].delete_one({"_id": _id}) for c in self.target_clients]
+                    *[
+                        c[TARGET_DB][coll_name].delete_one({"_id": _id})
+                        for c in self.target_clients
+                    ]
                 )
+            await self.source_client["globalDataReplication"]["status"].update_one(
+                {"_id": "resumeToken"}, {"$set": {"token": resume_token}},
+                upsert=True
+            )
 
             logger.info(f"Successfully processed {operation_type} operation")
         except Exception as e:
@@ -74,14 +88,20 @@ class ChangeStreamHandler:
 
     async def watch_changes(self):
         try:
-            self.change_stream = self.source_client[SOURCE_DB].watch([])
+            last_resume_token_doc = await self.source_client["globalDataReplication"]["status"].find_one({"_id": "resumeToken"})
+            last_resume_token = last_resume_token_doc.get("token") if last_resume_token_doc else None
+            if last_resume_token:
+                self.change_stream = self.source_client[SOURCE_DB].watch([], resume_after=last_resume_token)
+            else:
+                self.change_stream = self.source_client[SOURCE_DB].watch([])
+
             logger.info(f"Starting to watch changes on {SOURCE_DB}")
 
             async with self.change_stream as stream:
                 async for change in stream:
                     if self.should_stop:
                         break
-                    await self.write_to_target(change)
+                    await self.write_to_target(change, stream.resume_token)
 
         except Exception as e:
             logger.error(f"Error in change stream: {e}")
@@ -96,15 +116,17 @@ class ChangeStreamHandler:
         if self.source_client:
             self.source_client.close()
 
-        if self.target_client:
-            self.target_client.close()
+        if len(self.target_clients):
+            for c in self.target_clients:
+                c.close()
 
         logger.info("Cleanup completed")
 
 
 async def main():
-    handler = ChangeStreamHandler(source_uri=SOURCE_MONGODB_URI,
-                                  target_uris=TARGET_CLUSTERS_URIS)
+    handler = ChangeStreamHandler(
+        source_uri=SOURCE_MONGODB_URI, target_uris=TARGET_CLUSTERS_URIS
+    )
 
     loop = asyncio.get_running_loop()
     for sig in (signal.SIGINT, signal.SIGTERM):
